a0abb8de05127c405452db0ace66db1a9931113c
Shourabh1478 <shourabhmaloo@gmail.com>
2022-01-19 15:10:53 +0530

    appointment.js added

--- frontend/src/views/appointment/Appointment.js
+++ frontend/src/views/appointment/Appointment.js
@@ -3,6 +3,8 @@ import Navbar from '../layout/Navbar';
 import DatePicker from "react-datepicker";
 import { useState, useEffect } from 'react'
 import styled from 'styled-components'
+import Divider from '@mui/material/Divider';
+
 
 import "react-datepicker/dist/react-datepicker.css";
 
@@ -11,34 +13,57 @@ import "react-datepicker/dist/react-datepicker.css";
 function Appointment() {
 
   const [startDate, setStartDate] = useState(new Date());
+  const [description,setDescription]= useState("");
+  const [file,setFile]= useState([]);
+
+  // console.log(startDate);
+  // console.log(description);
+
+  const display = (e)=> {
+    e.preventDefault();
+  try {
+    // console.log("asncjnxaklncklm");
+    console.log(startDate);    
+    console.log(description);
+
+    console.log(file.name);
+  }catch(e) {
+    console.log(e);
+  }
+
+
+  }
+  
 
   return (
     <>
       <Navbar />
+      <form type="submit" onSubmit={display}>
       <div className="container">
         <div style={{ marginTop: "4rem" }} className="row">
           <div className="col s8 offset-s2">
             <div className="col s12" style={{ paddingLeft: "11.250px" }}>
               <h4>
-                <b>Book Your Appointmnet</b>
+                <b>Book Your Appointment</b>
               </h4>
             </div>
             <form>
               <div className="input-field col s12">
-                <input id="description" type="text" required/>
+                <input id="description" type="text" value={description} required onChange={(e)=>setDescription(e.target.value)}/>
                 <label htmlFor="description">Description</label>
-                {/* <span className="red-text">
-                  {errors.email}
-                  {errors.emailnotfound}
-                </span> */}
               </div>
               <div className="input-field col s12">
-                {/* <input id="date" type="text" required/>
-                <label htmlFor="date">Date</label> */}
                 <DatePicker selected={startDate} required onChange={(date) => setStartDate(date)} isClearable
                   placeholderText="I have been cleared!" closeOnScroll={true}      popperPlacement="bottom-end" 
                   />
               </div>
+              <div className="input-field col s12">
+                <input id="records" type="file" required style={{borderBottom: "1px solid #9e9e9e", width: "100%", padding:"10px 0"}} required onChange={(e)=>setFile(e.target.files[0])}/>
+              </div>
+
+
+
+
 
 
 
@@ -60,6 +85,7 @@ function Appointment() {
           </div>
         </div>
       </div>
+      </form>
 
     </>
   );
